07548d3d375c5ff0fbeb471366f7eb7514f95046
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 11:48:24 +0300

    add ResourceNotFound

---
+++ src/main/java/by/flameksandr/tasklist/domain/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package by.flameksandr.tasklist.domain.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
