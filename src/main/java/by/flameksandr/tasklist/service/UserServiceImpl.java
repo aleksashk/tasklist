adfa087bdea6c00f60da423e4978f799c83e1382
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 12:18:08 +0300

    add UserServiceImpl

---
+++ src/main/java/by/flameksandr/tasklist/service/UserServiceImpl.java
@@ -0,0 +1,35 @@
+package by.flameksandr.tasklist.service;
+
+import by.flameksandr.tasklist.domain.user.User;
+
+public class UserServiceImpl implements UserService {
+    @Override
+    public User getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User getByUserName(String username) {
+        return null;
+    }
+
+    @Override
+    public User update(User user) {
+        return null;
+    }
+
+    @Override
+    public User create(User user) {
+        return null;
+    }
+
+    @Override
+    public boolean idTaskOwner(Long userId, Long taskId) {
+        return false;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
