0926dbc817b29a128cad912897ef5d2e2aeeae5b
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 11:47:07 +0300

    add User

---
+++ src/main/java/by/flameksandr/tasklist/domain/user/User.java
@@ -0,0 +1,26 @@
+package by.flameksandr.tasklist.domain.user;
+
+import by.flameksandr.tasklist.domain.trask.Task;
+import lombok.Data;
+
+import java.util.List;
+import java.util.Set;
+
+@Data
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private String username;
+
+    private String password;
+
+    private String passwordConfirmation;
+
+    private Set<Role> roles;
+
+    private List<Task> tasks;
+
+}
