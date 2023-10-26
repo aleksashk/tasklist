096973483ccdde6877cb6c2a0022aac3db9364c3
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 11:49:45 +0300

    add AccessDeniedException

---
+++ src/main/java/by/flameksandr/tasklist/domain/exception/AccessDeniedException.java
@@ -0,0 +1,7 @@
+package by.flameksandr.tasklist.domain.exception;
+
+public class AccessDeniedException extends RuntimeException{
+    public AccessDeniedException(String message) {
+        super();
+    }
+}
