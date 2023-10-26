57e0f87768643039524da9fe29e985f8284ac684
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 12:17:36 +0300

    add TaskServiceImpl

---
+++ src/main/java/by/flameksandr/tasklist/service/TaskServiceImpl.java
@@ -0,0 +1,32 @@
+package by.flameksandr.tasklist.service;
+
+import by.flameksandr.tasklist.domain.trask.Task;
+
+import java.util.List;
+
+public class TaskServiceImpl implements TaskService{
+    @Override
+    public Task getById(Long id) {
+        return null;
+    }
+
+    @Override
+    public List<Task> getAllByUserId(Long id) {
+        return null;
+    }
+
+    @Override
+    public Task update(Task task) {
+        return null;
+    }
+
+    @Override
+    public Task create(Task task) {
+        return null;
+    }
+
+    @Override
+    public void delete(Long id) {
+
+    }
+}
