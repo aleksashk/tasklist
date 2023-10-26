bb16f6283e20245654dfbf5e0967d1b259394f2a
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 11:47:43 +0300

    add Role, Status

---
+++ src/main/java/by/flameksandr/tasklist/domain/trask/Status.java
@@ -0,0 +1,5 @@
+package by.flameksandr.tasklist.domain.trask;
+
+public enum Status {
+    TODO, IN_PROGRESS, DONE
+}
---
+++ src/main/java/by/flameksandr/tasklist/domain/user/Role.java
@@ -0,0 +1,5 @@
+package by.flameksandr.tasklist.domain.user;
+
+public enum Role {
+    ROLE_USER, ROLE_ADMIN
+}
