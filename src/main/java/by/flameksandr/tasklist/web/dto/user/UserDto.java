507d343e76d82eb844cf0619ff949c12ccb76b99
Aleksandr Philimonov <aleksandrphilimonov@gmail.com>
2023-10-26 12:48:30 +0300

    add validation to UserDto

--- src/main/java/by/flameksandr/tasklist/web/dto/user/UserDto.java
+++ src/main/java/by/flameksandr/tasklist/web/dto/user/UserDto.java
@@ -1,17 +1,30 @@
 package by.flameksandr.tasklist.web.dto.user;
 
-import by.flameksandr.tasklist.domain.trask.Task;
-import by.flameksandr.tasklist.domain.user.Role;
+import by.flameksandr.tasklist.web.dto.validation.OnCreate;
+import by.flameksandr.tasklist.web.dto.validation.OnUpdate;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Data;
-
-import java.util.List;
-import java.util.Set;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 public class UserDto {
+
+    @NotNull(message = "Id must be not null.", groups = OnUpdate.class)
     private Long id;
+
+    @NotNull(message = "Name must be not null.", groups = {OnUpdate.class, OnCreate.class})
+    @Length(max = 255, message = "Name length must be smaller than 255 symbols.", groups = {OnUpdate.class, OnCreate.class})
     private String name;
+
+    @NotNull(message = "Username must be not null.", groups = {OnUpdate.class, OnCreate.class})
+    @Length(max = 255, message = "Username length must be smaller than 255 symbols.", groups = {OnUpdate.class, OnCreate.class})
     private String username;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "Password must be not null.", groups = {OnUpdate.class, OnCreate.class})
     private String password;
-    private String passwordConfirmation;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull(message = "PasswordConfirmation must be not null.", groups = {OnCreate.class})private String passwordConfirmation;
 }
